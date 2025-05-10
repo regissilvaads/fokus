f245c741debbfa2769135ee9522ac222650fa2f9
Regis <regis.silva.ads@gmail.com>
2025-05-10 16:39:27 -0300

    adicionado persistencia do formulário de tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
+const textAreaListTask = document.querySelector('.app__form-textarea');
 
 btnAddTask.addEventListener('click', () => {
   formAddTask.classList.toggle('hidden');
+});
+
+const taskList = [];
+
+formAddTask.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  const task = {
+    description: textAreaListTask.value,
+  };
+  taskList.push(task);
+  localStorage.setItem('taskList', JSON.stringify(taskList));
 });
