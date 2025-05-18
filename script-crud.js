dbdab166899faa5849fbb3f36a2f309a8847d934
Regis <regis.silva.ads@gmail.com>
2025-05-18 10:17:10 -0300

    Adicionado elemento de apresentação da lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,12 +1,43 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
 const textAreaListTask = document.querySelector('.app__form-textarea');
+const ulTask = document.querySelector('.app__section-task-list');
+
+const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
 
 btnAddTask.addEventListener('click', () => {
   formAddTask.classList.toggle('hidden');
 });
 
-const taskList = [];
+function newTaskElement(task) {
+  const li = document.createElement('li');
+  li.classList.add('app__section-task-list-item');
+
+  const svg = document.createElement('svg');
+  svg.innerHTML = `
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+    <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+  </svg>
+  `;
+
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('app__section-task-list-item-description');
+  paragraph.textContent = task.description;
+
+  const button = document.createElement('button');
+  button.classList.add('app_button-edit');
+  const imageButton = document.createElement('img');
+
+  imageButton.setAttribute('src', '/imagens/edit.png');
+  button.append(imageButton);
+
+  li.append(svg);
+  li.append(paragraph);
+  li.append(button);
+  return li;
+}
 
 formAddTask.addEventListener('submit', (evento) => {
   evento.preventDefault();
@@ -14,5 +45,13 @@ formAddTask.addEventListener('submit', (evento) => {
     description: textAreaListTask.value,
   };
   taskList.push(task);
+  const taskElement = newTaskElement(task);
+  ulTask.append(taskElement);
   localStorage.setItem('taskList', JSON.stringify(taskList));
+  textAreaListTask.value = '';
+  formAddTask.classList.add('hidden');
+});
+taskList.forEach((task) => {
+  const taskElement = newTaskElement(task);
+  ulTask.append(taskElement);
 });
